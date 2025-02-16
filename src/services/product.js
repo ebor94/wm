7f6cf4b407a159f3944698b2a57cb07224edf30c
ebor94 <ebor94@hotmail.com>
2025-02-16 18:23:17 -0500

    add service product for get info etiqueta

---
+++ src/services/product.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const domain = "https://lilix.ceramicaitalia.com:3001";
+let headers = { "Content-Type": "application/json" }
+
+export const infoEstiba = { 
+
+    async getInfoEtiqueta(pallet) {
+        return axios.get(
+          `${domain}/producto/etiqueta/${pallet}`,
+          { headers: headers }
+        );
+      },
+
+
+}
