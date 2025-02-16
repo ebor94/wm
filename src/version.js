e05718919e8e228b9579bea56e98abfed3533be8
ebor94 <ebor94@hotmail.com>
2025-02-16 18:32:11 -0500

    version que elimina la etiqueta manual

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 
-export const version = '2025.01.23';
-export const buildNumber = 24;
-export const buildDate = '2025-01-23T21:47:28.216Z';
+export const version = '2025.02.14';
+export const buildNumber = 26;
+export const buildDate = '2025-02-14T21:23:05.397Z';
 export const environment = 'production';
-export const fullVersion = '2025.01.23-build.24';
+export const fullVersion = '2025.02.14-build.26';
