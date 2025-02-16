929a3ca5497716c7ed551415a4a7f59765802ae3
ebor94 <ebor94@hotmail.com>
2025-02-16 18:35:45 -0500

    Solicito Soporte de Tecnologia - 02.752.848

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 
-export const version = '2025.02.14';
-export const buildNumber = 26;
-export const buildDate = '2025-02-14T21:23:05.397Z';
+export const version = '2025.02.16';
+export const buildNumber = 28;
+export const buildDate = '2025-02-16T23:32:37.658Z';
 export const environment = 'production';
-export const fullVersion = '2025.02.14-build.26';
+export const fullVersion = '2025.02.16-build.28';
