072fa088ea7deb8ad063161425fd778eda8d091b
ebor94 <ebor94@hotmail.com>
2025-03-07 11:08:24 -0500

    change production por prod

--- src/version.js
+++ src/version.js
@@ -2,5 +2,5 @@
 export const version = '2025.03.03';
 export const buildNumber = 32;
 export const buildDate = '2025-03-04T00:34:42.628Z';
-export const environment = 'production';
+export const environment = 'prod';
 export const fullVersion = '2025.03.03-build.32';
