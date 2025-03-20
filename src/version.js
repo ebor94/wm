8558fcc950dfa94fedae3df5b700144ac9aeccb7
ebor94 <ebor94@hotmail.com>
2025-03-20 14:01:03 -0500

    change version 42

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 
-export const version = '2025.03.07';
-export const buildNumber = 34;
-export const buildDate = '2025-03-07T16:09:23.850Z';
+export const version = '2025.03.13';
+export const buildNumber = 42;
+export const buildDate = '2025-03-14T01:55:05.439Z';
 export const environment = 'production';
-export const fullVersion = '2025.03.07-build.34';
+export const fullVersion = '2025.03.13-build.42';
