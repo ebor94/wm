21ec5653cd43dec6247e17ec3cd2616de92f2372
ebor94 <webmaster@ceramicaitalia.com>
2025-03-28 16:21:15 -0500

    version modal change store

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 
 export const version = '2025.03.28';
-export const buildNumber = 48;
-export const buildDate = '2025-03-28T12:50:29.687Z';
+export const buildNumber = 50;
+export const buildDate = '2025-03-28T21:20:36.219Z';
 export const environment = 'production';
-export const fullVersion = '2025.03.28-build.48';
+export const fullVersion = '2025.03.28-build.50';
