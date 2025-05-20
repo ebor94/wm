40b1fd2a2595a12e7a1a6e9de02d0632f458da63
ebor94 <ebor94@hotmail.com>
2025-05-20 10:58:56 -0500

    new version

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 
-export const version = '2025.04.28';
-export const buildNumber = 52;
-export const buildDate = '2025-04-28T22:43:42.467Z';
+export const version = '2025.05.20';
+export const buildNumber = 56;
+export const buildDate = '2025-05-20T15:56:43.607Z';
 export const environment = 'production';
-export const fullVersion = '2025.04.28-build.52';
+export const fullVersion = '2025.05.20-build.56';
