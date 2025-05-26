e8f7d54fc75a797b97ab9b21c197a8ba54e21f96
ebor94 <webmaster@ceramicaitalia.com>
2025-05-26 07:57:20 -0500

    new version

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 
-export const version = '2025.05.23';
-export const buildNumber = 62;
-export const buildDate = '2025-05-23T16:32:17.607Z';
+export const version = '2025.05.24';
+export const buildNumber = 64;
+export const buildDate = '2025-05-24T13:52:59.323Z';
 export const environment = 'production';
-export const fullVersion = '2025.05.23-build.62';
+export const fullVersion = '2025.05.24-build.64';
